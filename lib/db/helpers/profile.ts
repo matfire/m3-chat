6291f6095dd8e0da0db46c5bf2078a5747e29e02
Matteo Gassend <mgassend@gmail.com>
2025-06-15 17:52:55 +0200

    fix: use transaction and "onFailDoNothing"

--- lib/db/helpers/profile.ts
+++ lib/db/helpers/profile.ts
@@ -3,11 +3,13 @@ import { db } from "..";
 import { profile } from "../schemas";
 
 export async function getOrCreateProfile(userId: string) {
-    const existingProfile = await db.query.profile.findFirst({where: eq(profile.userId, userId)})
-    if (existingProfile) return existingProfile
-    const newProfile = await db.insert(profile).values({
-        userId,
-        data: {}
-    }).returning()
-    return newProfile[0]
+    return await db.transaction(async(tx) => {
+        const existingProfile = await db.query.profile.findFirst({where: eq(profile.userId, userId)})
+        if (existingProfile) return existingProfile
+        const newProfile = await db.insert(profile).values({
+            userId,
+            data: {}
+        }).onConflictDoNothing().returning()
+        return newProfile[0]
+    })
 }
