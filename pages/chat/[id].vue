bb9eab9f5cec2102fda75a26a3b4e54da0f2866c
Matteo Gassend <mgassend@gmail.com>
2025-06-18 17:50:38 +0200

    add: layout change

--- pages/chat/[id].vue
+++ pages/chat/[id].vue
@@ -83,32 +83,30 @@ const handleSubmit = async (value: string) => {
             <SidebarTrigger />
             <h1 class="text-2xl font-bold">{{ title }}</h1>
         </div>
-        <div class="flex flex-col space-y-4 flex-1 w-full">
+        <div class="flex flex-col space-y-6 flex-1 w-full">
             <div v-if="data && 'messages' in data" v-for="message in messages" :key="message.id"
-                :class="{ 'justify-end': message.sender === 'assistant', 'w-full flex': true }">
-                <Card class="w-3/4">
+                class="w-full flex">
+                <Card v-if="message.sender === 'user'">
                     <CardContent class="p-4">
-                        <p v-if="message.sender === 'user'" class="">
-                            {{ message.content }}
-                        </p>
-                        <div v-if="message.sender === 'assistant'">
-                            <div v-if="message.reasoning">
-                                <Accordion type="single" collapsible>
-                                    <AccordionItem :value="message.id + '-reasoning'">
-                                        <AccordionTrigger>Reasoning</AccordionTrigger>
-                                        <AccordionContent>
-                                            <div class="w-full text-wrap space-y-2" v-html="message.rendererReasoning" v-if="message.rendererReasoning" />
-                                            <MdRenderer v-else-if="message.reasoning" :content="message.reasoning" />
-                                        </AccordionContent>
-                                    </AccordionItem>
-                                </Accordion>
-                            </div>
-                            <div class="space-y-2" v-html="message.rendererContent" v-if="message.rendererContent" />
-                            <MdRenderer v-else-if="message.content" :content="message.content" />
-                        </div>
-                        <Loader2 class="animate-spin" v-if="message.status === 'generating'" />
+                        {{ message.content }}
                     </CardContent>
                 </Card>
+                <div v-else class="w-full px-4">
+                    <div v-if="message.reasoning">
+                        <Accordion type="single" collapsible>
+                            <AccordionItem :value="message.id + '-reasoning'">
+                                <AccordionTrigger class="flex justify-start"><span>Reasoning</span><Loader2 v-if="message.reasoning && !message.content" class="animate-spin" /></AccordionTrigger>
+                                <AccordionContent>
+                                    <div class="w-full text-wrap space-y-2" v-html="message.rendererReasoning" v-if="message.rendererReasoning" />
+                                    <MdRenderer v-else-if="message.reasoning" :content="message.reasoning" />
+                                </AccordionContent>
+                            </AccordionItem>
+                        </Accordion>
+                    </div>
+                    <div class="space-y-2" v-html="message.rendererContent" v-if="message.rendererContent" />
+                    <MdRenderer v-else-if="message.content" :content="message.content" />
+                    <Skeleton v-if="message.status === 'generating'" class="mt-2 w-full h-10 rounded-md" />
+                </div>
             </div>
         </div>
         <div>
