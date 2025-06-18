bf9f6ab055594ddb63ef6c70dad33b9d7f09fc83
Matteo Gassend <mgassend@gmail.com>
2025-06-18 12:05:05 +0200

    all: sort

--- server/api/chat/all.get.ts
+++ server/api/chat/all.get.ts
@@ -1,10 +1,10 @@
-import { eq } from "drizzle-orm"
+import { desc, eq } from "drizzle-orm"
 import { db } from "~/lib/db"
 import { chat } from "~/lib/db/schemas"
 import defineAuthenticatedEventHandler from "~/utils/define-authenticated-event-handler"
 
 export default defineAuthenticatedEventHandler(async(event) => {
 
-    const data = await db.select({id: chat.id, title: chat.title}).from(chat).where(eq(chat.userId, event.context.user.id))
+    const data = await db.select({id: chat.id, title: chat.title}).from(chat).where(eq(chat.userId, event.context.user.id)).orderBy(desc(chat.updatedAt))
     return data
 })
